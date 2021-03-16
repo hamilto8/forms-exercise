1c76e344cd093e86fb0661d1583a476106e18af7
Michael <michael.ts.hamilton@gmail.com>
2021-03-16 15:37:32 -0500

    Refactored alert message into separate function. Added success notification.

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ const password = document.querySelector('#password');
 const passwordConfirmation = document.querySelector('#password-confirmation');
 const submitBtn = document.querySelector('#submit-btn');
 const alertDiv = document.querySelector('.alert-div');
+const successDiv = document.querySelector('.success-div');
 
 const form = document.querySelector('.form');
 const email = form.querySelector('#email');
@@ -10,32 +11,47 @@ const zipCode = form.querySelector('#zip-code');
 
 submitBtn.addEventListener('click', submitForm);
 
+function alertMessage(msg){
+    alertDiv.style.display = 'block';
+        const warningMsgH4 = document.createElement('h4');
+            warningMsgH4.innerText = `${msg}`;
+            warningMsgH4.style.color = 'red';
+            alertDiv.appendChild(warningMsgH4);
+            setTimeout(() => {
+                alertDiv.removeChild(warningMsgH4);
+                alertDiv.style.display = 'none';
+            }, 1000);
+}
+
+function checkForm(){
+    if(email.value.toString().length < 5){
+        alertMessage('Invalid Email');
+        return false;
+    } 
+    if(country.toString().length === 0){
+        alertMessage('Please Select a Country');
+        return false;
+    }
+    if(zipCode.value.toString().length < 5){
+        alertMessage('Please enter valid zip code.');
+        return false;
+    }
+    if(!passwordsMatch()){
+        return false;
+    }
+    return true;
+}
+
+
+
 function passwordsMatch(){
 
     if(password.value.toString().length === 0){
         console.log('No password entered');
-        alertDiv.style.display = 'block';
-        const warningMsgH4 = document.createElement('h4');
-            warningMsgH4.innerText = 'No Password Entered';
-            warningMsgH4.style.color = 'red';
-            alertDiv.appendChild(warningMsgH4);
-            setTimeout(() => {
-                alertDiv.removeChild(warningMsgH4);
-                alertDiv.style.display = 'none';
-            }, 1000);
+        alertMessage('No Password Entered');
         return false;
     } else if (password.value.toString().length < 8){
-        console.log('Passwords must be at least 8 characters long');
-        alertDiv.style.display = 'block';
-        const warningMsgH4 = document.createElement('h4');
-            warningMsgH4.innerText = 'Passwords must be at least 8 characters long';
-            warningMsgH4.style.color = 'red';
-            alertDiv.appendChild(warningMsgH4);
-            setTimeout(() => {
-                alertDiv.removeChild(warningMsgH4);
-                alertDiv.style.display = 'none';
-            }, 1000);
-
+        alertMessage('Passwords must be at least 8 characters long');
         return false;
     }
 
@@ -44,37 +60,31 @@ function passwordsMatch(){
         console.log('Passwords match');
         return true;
     } else {
-        console.log('Password must match!');
-        alertDiv.style.display = 'block';
-        const warningMsgH4 = document.createElement('h4');
-            warningMsgH4.innerText = 'Password must match!';
-            warningMsgH4.style.color = 'red';
-            alertDiv.appendChild(warningMsgH4);
-            setTimeout(() => {
-                alertDiv.removeChild(warningMsgH4);
-                alertDiv.style.display = 'none';
-            }, 1000);
+        alertMessage('Password must match!');
         return false;
     }
 }
 
+function submissionSuccess(){
+    successDiv.style.display = 'block';
+    form.style.opacity = '0.1';
+    setTimeout(() => {
+        successDiv.style.display = 'none';
+        form.style.opacity = '1';
+    }, 1000);
+
+}
+
 function submitForm(e){
     e.preventDefault();
-    if(passwordsMatch()){
-        console.log('Passwords match. Submitting...');
-        console.log(`
-            Email: ${email.value},
-            Country: ${country.value},
-            Zip Code: ${zipCode.value},
-            Password: ${password.value}
-        `);
-
+    if(checkForm()){
+        submissionSuccess();
         email.value = '';
         country.value = '';
         zipCode.value = '';
         password.value = '';
         passwordConfirmation.value = '';
-
-        return;
-    } 
+    }
+    
+    return;
 }
